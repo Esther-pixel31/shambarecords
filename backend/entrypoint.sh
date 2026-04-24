59ce2a25923fcb07cec006bbda17fbbcb2765f40
Esther Wanza Mutua <esthermutua16@gmail.com>
2026-04-25 01:28:45 +0300

    change entry point to production mode

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,26 +1,21 @@
 #!/bin/sh
 set -e
 
-echo "Waiting for PostgreSQL..."
+echo "Waiting for DB..."
 
 until python - <<'PY'
 import os
 import psycopg2
-
 conn = psycopg2.connect(os.environ["DATABASE_URL"])
 conn.close()
 PY
 do
-  echo "PostgreSQL not ready"
   sleep 2
 done
 
 echo "Running migrations..."
 export FLASK_APP=app.main:create_app
 python -m flask db upgrade
-
-echo "Seeding database..."
-python -m app.seed
 
 echo "Starting server..."
 gunicorn "app.main:create_app()" --bind 0.0.0.0:10000
